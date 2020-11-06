18dcbacf6ac0bd4257fea6c2bbe4bef599e58e19
keeYuc <52500690@qq.com>
2020-11-06 19:16:26 +0800

    main

--- build/myLib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keeyu/mycode/CMakeTest/CMakeTest/myLib
-# Build directory: /home/keeyu/mycode/CMakeTest/CMakeTest/build/myLib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
